f79d66873a219f83c3583b9dc1cf754e36ec6700
Axcelaw <walace.andr.m@gmail.com>
2023-01-18 16:04:29 -0300

    Created button click event handler for ExpenseItem

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import Card from '../UI/Card';
 import ExpenseItem from './ExpenseItem';
-import './Expenses.css'
+import './Expenses.css';
 
-const Expenses = props => {
+const Expenses = (props) => {
   return (
     <Card className="expenses">
       <ExpenseItem
@@ -27,6 +27,6 @@ const Expenses = props => {
       />
     </Card>
   );
-}
+};
 
 export default Expenses;
